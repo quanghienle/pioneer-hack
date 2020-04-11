219158ce9a9c7347b432f0465a5c1a3c7da9d9b9
quanghienle <quanghien.a1@gmail.com>
2020-04-11 18:26:46 -0400

    fixed sidebar to be functional

---
+++ client/src/components/TrendingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class TrendingPage extends React.Component {
+  state = {
+    data: null,
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Trending Page</p>
+      </div>
+    );
+  }
+}
+
+export default TrendingPage;
