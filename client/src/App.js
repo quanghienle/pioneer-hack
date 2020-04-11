0b300977d8bb3d31bc7ed012e0a2b9be6c38bbbb
quanghienle <quanghien.a1@gmail.com>
2020-04-11 16:21:40 -0400

    Bodejs and REact

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends React.Component {
+  state = {
+    data: null,
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then((res) => console.log(res.express))
+      .catch((err) => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch("/express_backend");
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <SignIn />
+            </Route>
+            <Route exact path="/home">
+              {/* <Main /> */}
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # Pioneer hack
 
 This is the first commit
+
+# Instruction
+
+- Run server
+    `$ npm install` (maybe)
+    `$ node server.js`
+
+- Run client
+    `$ npm install` (maybe)
+    `$ cd client && yarn start`
